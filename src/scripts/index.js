f7ac246a946a18c078c02d88d3cb3e0efa9ad738
Saleh Laham <laham@list.ru>
2021-02-27 18:04:34 +0300

    First changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,31 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 console.log('webpack starterkit');
+
+const btnMenu = document.querySelector(".header__btn-menu");
+const headerUl = document.querySelector(".header__ul");
+const btnTheme = document.querySelector(".header__btn-theme");
+const page = document.querySelector(".theme");
+const spanText = document.querySelector(".main__wrapper__content__span");
+const menuSpan = document.querySelector(".header__btn-menu span");
+
+btnMenu.onclick = function () {
+  menuSpan.classList.toggle("active");
+  btnMenu.classList.toggle("active");
+  headerUl.classList.toggle("header__ul");
+  headerUl.classList.toggle("header__ul-mob");
+};
+
+btnTheme.onclick = function () {
+  page.classList.toggle("theme");
+  page.classList.toggle("theme-dark");
+  spanText.classList.toggle("main__wrapper__content__span");
+  spanText.classList.toggle("main__wrapper__content__span_dark");
+};
+
+$(".autoplay").slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+});
